01e641b80cd2346125f42d72f9668feb13e48ec6
samir <elassal@in.tum.de>
2020-04-20 18:25:16 +0200

    uploading everything

---
+++ ExceptionReproduction/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExceptionReproduction
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public static List<object> garbage = new List<object>();
+
+        void PanGestureRecognizer_PanUpdated(System.Object sender, Xamarin.Forms.PanUpdatedEventArgs e)
+        {
+            lbl.GestureRecognizers.Clear();
+            stk.Children.Remove(lbl);
+            garbage.Add(lbl);
+        }
+
+        void btn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            lbl.GestureRecognizers.Clear();
+            stk.Children.Remove(lbl);
+            garbage.Add(lbl);
+        }
+    }
+}
